899cc857fa53b0f001362f306c422537a7f62101
Omar Bahareth <omar@omar.engineer>
2019-01-29 23:06:11 +0300

    :recycle: Convert to a CLI app using commander.js
    Fixes #1.

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const ModulesChecker = require('./src/modules-checker')
+
+program
+  .version('0.1.0')
+  .command('check <path>')
+  .description('Checks if all node_modules at <path> are ES5')
+  .option('-a, --all', 'Log all messages (including modules that are ES5)')
+  .action((path, cmd) => {
+    const config = ModulesChecker.defaultConfig
+
+    if (cmd.all) {
+      config = {
+        logEs5Packages: true
+      }
+    }
+
+    const checker = new ModulesChecker(path, config)
+    checker.checkModules();
+  })
+
+
+program.parse(process.argv)
---
+++ src/modules-checker.js
@@ -0,0 +1,70 @@
+const { exec } = require('child_process')
+
+const path = require('path')
+
+class ModulesChecker {
+  constructor(dir, config = ModulesChecker.defaultConfig) {
+    this.config = config
+    this.dir = dir
+  }
+
+  checkModules() {
+    const nodeModulesDir = path.join(this.dir, 'node_modules')
+    const dependencies = this.getDepsFromPackageJson()
+
+    if (!dependencies) {
+      return
+    }
+
+    dependencies.forEach(dependency => {
+      const packagePath = path.join(nodeModulesDir, dependency)
+      const packageJson = require(path.join(packagePath, 'package.json'))
+
+      if (packageJson.main) {
+        const mainScriptPath = path.join(packagePath, packageJson.main)
+
+        this.checkScript(mainScriptPath, dependency)
+      }
+    })
+  }
+
+  getDepsFromPackageJson() {
+    const packageJsonPath = path.join(this.dir, 'package.json')
+    const packageJson = require(packageJsonPath)
+
+    if (!packageJson) {
+      console.error(`Failed to load package.json in ${this.dir}`)
+      return null
+    }
+
+    return Object.keys(packageJson.dependencies)
+  }
+
+  checkScript(scriptPath, dependencyName) {
+    // TODO: Check all scripts this script requires/imports
+    exec(`es-check es5 ${scriptPath}`, (error, stdout, stderr) => {
+      if (error) {
+        if (error.message.includes('ES version matching errors')) {
+          console.log(`❌ ${dependencyName} is not ES5`)
+        }
+
+        // node couldn't execute the command
+        else {
+          console.error(error)
+        }
+
+        return
+      }
+
+      if (this.config.logEs5Packages) {
+        console.log(`✅ ${dependencyName} is ES5`)
+      }
+    })
+  }
+}
+
+ModulesChecker.defaultConfig = {
+  logEs5Packages: false
+}
+
+module.exports = ModulesChecker
