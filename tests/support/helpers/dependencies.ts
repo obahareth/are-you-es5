f34bda6c417fe1a3efd6cff627cf0269ef90c3b2
Omar Bahareth <omar@omar.engineer>
2019-07-27 17:59:32 +0300

    :white_check_mark: Move test dependencies to their own file, and declare a new t

---
+++ tests/support/helpers/dependencies.ts
@@ -0,0 +1,32 @@
+import path from 'path'
+import IDependencyWithPathAndOutput from "../types/dependency-with-path-and-output";
+
+export const directDependencies = [
+  'acorn',
+  'commander',
+  'is-even',
+  'react',
+  'uid',
+  'underscore',
+  'whatwg-fetch'
+]
+
+export const allDependencies = [
+  'acorn',
+  'commander',
+  'is-buffer',
+  'is-even',
+  'is-number',
+  'is-odd',
+  'js-tokens',
+  'kind-of',
+  'loose-envify',
+  'object-assign',
+  'prop-types',
+  'react',
+  'react-is',
+  'scheduler',
+  'uid',
+  'underscore',
+  'whatwg-fetch'
+]
