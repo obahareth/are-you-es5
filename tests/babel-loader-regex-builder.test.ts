8ff5a0bb4b7e0a6c4b111dd3dcad76ecb4c89cca
Omar Bahareth <omar@omar.engineer>
2019-10-29 20:38:26 +0300

    :bug: Escape namespaced dependencies in Babel exclude regex
    Handle dependencies with a forward slash in their name by replacing it with a `\/` so they can work properly in a regex. This should fix #20.

---
+++ tests/babel-loader-regex-builder.test.ts
@@ -0,0 +1,17 @@
+import { getBabelLoaderIgnoreRegex } from "../src/babel-loader-regex-builder"
+
+describe('getBabelLoaderIgnoreRegex', () => {
+  it("returns a regex for ignoring dependencies", () => {
+    const dependencies = ["dotenv", "md5-file",]
+    const expectedRegex = "/[\\\\/]node_modules[\\\\/](?!(dotenv|md5-file)[\\\\/])/"
+
+    expect(getBabelLoaderIgnoreRegex(dependencies)).toEqual(expectedRegex)
+  })
+
+  it("handles namespaced dependencies", () => {
+    const dependencies = ["@react-pdf/renderer", "dotenv", "md5-file",]
+    const expectedRegex = "/[\\\\/]node_modules[\\\\/](?!(@react-pdf\\/renderer|dotenv|md5-file)[\\\\/])/"
+
+    expect(getBabelLoaderIgnoreRegex(dependencies)).toEqual(expectedRegex)
+  })
+})
