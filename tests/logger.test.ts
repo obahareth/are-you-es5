140a06cd49fec100fc7e7ac6fc2337cb7193af16
Omar Bahareth <omar@omar.engineer>
2020-05-25 04:42:02 +0300

    :ok_hand: Update Logger Tests based on Review Feedback
    Reorganize tests based on [this comment](https://github.com/obahareth/are-you-es5/pull/40#discussion_r428125858).

--- tests/logger.test.ts
+++ tests/logger.test.ts
@@ -1,20 +1,7 @@
 import { Logger } from '../src/logger'
 
-describe('when silent config is present', () => {
-  it('should not log', () => {
-    const spy = jest.spyOn(global.console, 'log')
-    const logger = new Logger({ silent: true })
-    const message = 'Hello, have you received my call?'
-
-    logger.log(message)
-
-    expect(console.log).not.toHaveBeenLastCalledWith(message)
-    spy.mockRestore()
-  })
-})
-
-describe('when silent config is not present', () => {
-  it('should log', () => {
+describe('Logger', () => {
+  it('should log by default', () => {
     const spy = jest.spyOn(global.console, 'log')
     const logger = new Logger({ silent: false })
     const message = 'Hello, have you received my call?'
@@ -22,6 +9,17 @@ describe('when silent config is not present', () => {
     logger.log(message)
 
     expect(console.log).toHaveBeenLastCalledWith(message)
+    spy.mockRestore()
+  })
+
+  it('should do nothing when configured to be silent', () => {
+    const spy = jest.spyOn(global.console, 'log')
+    const logger = new Logger({ silent: true })
+    const message = 'Hello, have you received my call?'
+
+    logger.log(message)
+
+    expect(console.log).not.toHaveBeenLastCalledWith(message)
     spy.mockRestore()
   })
 })
